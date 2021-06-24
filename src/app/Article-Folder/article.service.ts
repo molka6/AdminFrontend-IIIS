a73dc1a76bab96621b60dba52f141970d32b8a7d
Molka <molkabenameur6@gmail.com>
2021-06-24 21:22:57 +0200

    first

---
+++ src/app/Article-Folder/article.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+import axios  from 'axios';
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+  url = 'http://localhost:8000';
+  constructor() { }
+ async getArticles() {
+    return await axios.get( ' http://localhost:8000' + "/offreEmploi") ; 
+  }
+}
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuHeadComponent } from './menu-head-folder/menu-head/menu-head.component';
+import { MenuLeftComponent } from './menu-left-folder/menu-left/menu-left.component';
+import { ArticleComponent } from './article-Folder/article/article.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuHeadComponent,
+    MenuLeftComponent,
+    ArticleComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
