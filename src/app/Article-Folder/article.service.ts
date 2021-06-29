80e757b1d1f4866f856bc49966898b1282a0c0fc
Molka <molkabenameur6@gmail.com>
2021-06-29 14:33:49 +0200

    molka

--- src/app/Article-Folder/article.service.ts
+++ src/app/Article-Folder/article.service.ts
@@ -1,15 +1,33 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
 import axios  from 'axios';
+
 @Injectable({
   providedIn: 'root'
 })
 
 export class ArticleService {
   url = 'http://localhost:8000';
-  constructor() { }
+
+
+  constructor(private http : HttpClient ,private  headers : Headers) { }
+
+
  async getArticles() {
     return await axios.get( ' http://localhost:8000' + "/Article") ; 
   }
+
+
+
+
+
+}
+
+export interface Article {
+  id: Number,
+  title: String,
+  description: string,
+  DateAjout: string
 }
 
